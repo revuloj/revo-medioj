f2c3329ff75c962c63c302dcfd57e52ea1844848
Wolfram Diestel <diestel@steloj.de>
2019-05-11 16:06:10 +0200

    pliaj testoj

--- ĝardeno/formikujo/tst/formikujo-testoj.sh
+++ ĝardeno/formikujo/tst/formikujo-testoj.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
 echo
-echo "ĉu formiko estas aktiva? Ni bezonas la n-ron..."
+echo "### Ĉu formiko estas aktiva? Ni bezonas la n-ron..."
 formiko_id=$(docker ps --filter name=formikujo_formiko -q) && echo ${formiko_id}
 
 echo
-echo "testo de la komunikado inter formiko -> afido per ssh..."
+echo "### Testo de la komunikado inter formiko -> afido per ssh..."
 docker exec -u1001 -it  ${formiko_id} ssh -i /run/secrets/voko-formiko.ssh_key -o StrictHostKeyChecking=no \
     -o PasswordAuthentication=no afido@afido ls -l /usr/local/bin/processmail.pl
+
+echo
+echo "### Testo de ant redaktoservo..."
+docker exec -u1001 -it ${formiko_id} bash -c "ant -f \${VOKO}/ant/redaktoservo.xml -p"
+docker exec -u1001 -it ${formiko_id} bash -c "cd \${REVO}; ant -f \${VOKO}/ant/redaktoservo.xml srv-agordo"
+docker exec -u1001 -it ${formiko_id} bash -c "cd \${REVO}; ant -f \${VOKO}/ant/redaktoservo.xml srv-shlosu"
+docker exec -u1001 -it ${formiko_id} bash -c "cd \${REVO}; ant -f \${VOKO}/ant/redaktoservo.xml srv-malshlosu"
