035556f404ee56e09303673a657d7e87436eb94d
Wolfram Diestel <diestel@steloj.de>
2019-05-20 22:28:59 +0200

    helpaj skriptoj

---
+++ ĝardeno/formikujo/formiko-exec.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+### Ĉu Afido estas aktiva? Ni bezonas la n-ron...
+formiko_id=$(docker ps --filter name=formikujo_formiko -q) && echo "Formiko: ${formiko_id}"
+
+exec docker exec -it -u1001 ${formiko_id} $@
+#docker exec -it -u1001 ${formiko_id} $@
+
--- ĝardeno/formikujo/formikujo-deploy.sh
+++ ĝardeno/formikujo/formikujo-deploy.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 
 docker stack deploy -c docker-compose.yml formikujo
+
+# ĉu sufiĉas aliokaze skribu maŝon, kiu reprovas plurfoje...
+sleep 10
+
+afido_id=$(docker ps --filter name=formikujo_afido -q) && echo "Afido: ${afido_id}"
+tomocero_id=$(docker ps --filter name=formikujo_tomocero -q) && echo "Tomocero: ${tomocero_id}"
+formiko_id=$(docker ps --filter name=formikujo_formiko -q) && echo "Formiko: ${formiko_id}"
+
+# Plibonigu: kiel ni povas montri tion en "bats"?
+if [ "${afido_id}" = "" ]; then echo "Afido ne aktiva!" 1>&2; exit 1; fi
+if [ "${tomocero_id}" = "" ]; then echo "Tomocero ne aktiva!" 1>&2; exit 1; fi
+if [ "${formiko_id}" = "" ]; then echo "Formiko ne aktiva!" 1>&2; exit 1; fi
