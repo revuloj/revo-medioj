1d4839a198c2786a3e1745c003faa884a54e31d6
Wolfram Diestel <diestel@steloj.de>
2020-04-07 18:34:11 +0200

    afido-testo

---
+++ afido-testo/deploy.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+#set -x
+
+stack=afidotesto
+nw=$(docker network ls --filter name=${stack}_default -q)
+#formiko_id=$(docker ps --filter name=formikujo_formiko -q)
+#echo "nw: $nw"
+#echo "fm: $formiko_id"
+#if [[ ( ! ${nw} = "" && ${formiko_id} = "" ) ]]; then
+if [[ ! ${nw} = "" ]]; then
+  echo "${stack}_default: $nw"
+  echo "Ankoraŭ ekzistas reto '${stack}_default.'"
+  echo "Post 'docker stack rm ${stack}' necesos iom atendi"
+  echo "ĝis la reto foriĝas. Bv. reprovi poste."
+  exit 1
+fi
+
+docker stack deploy -c docker-compose.yml ${stack}
+
+# ĉu sufiĉas aliokaze skribu maŝon, kiu reprovas plurfoje...
+sleep 10
+
+#afido_id=$(docker ps --filter name=${stack}_afido -q) && echo "Afido: ${afido_id}"
+#tomocero_id=$(docker ps --filter name=${stack}_tomocero -q) && echo "Tomocero: ${tomocero_id}"
+afido_id=$(docker ps --filter name=${stack}_afido -q) && echo "Afido: ${afido_id}"
+
+# Plibonigu: kiel ni povas montri tion en "bats"?
+#if [ "${afido_id}" = "" ]; then echo "Afido ne aktiva!" 1>&2; exit 1; fi
+#if [ "${tomocero_id}" = "" ]; then echo "Tomocero ne aktiva!" 1>&2; exit 1; fi
+if [ "${afido_id}" = "" ]; then echo "Afido ne aktiva!" 1>&2; exit 1; fi
+
+docker service logs ${stack}_afido 
+
+# wait
---
+++ afido-testo/voko-afido-sekretoj.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+# access_token ni uzas por aliri Github-API nome de reta-vortaro
+access_token=$1
+
+# tiun ŝlosilon ni uzas, por ke formiko povas voko afidon, verŝajne ni ne bezonas tion plu...
+#keyfile_pub=${HOME}/.ssh/formiko.pub
+
+# tiun ŝlosilon ni uzas por sendi ŝanĝojn al la Git-arĥivo (revo-fonto-testo...)
+keyfile_github=${HOME}/.ssh/id_rsa_revo_test
+
+if [[ -z ${access_token} ]]; then
+  echo "Vi devas doni ĵetonon de Github ('personal access token') kiel argumento por krei sekreton."
+  exit 1
+fi  
+
+#if [ ! -e ${keyfile_pub} ]; then
+#    echo "Mankas ${keyfile_pub}. Vi devas unue lanĉi voko-formiko-sekretojn por ricevi ŝlosilparon!"
+#    exit 1
+#fi
+
+if [ ! -e ${keyfile_github} ]; then
+    echo "Mankas ${keyfile_github}. Vi bezonas ŝlosilon por povi puŝi ŝangojn al github.com - demandu Revo-administranton!"
+    exit 1
+fi
+
+secrets=$(docker secret ls --filter name=voko-afido. -q)
+if [ ! -z "${secrets}" ]; then
+    echo "# forigante malnovajn sekretojn voko-afido.* ..."
+    docker secret rm ${secrets}
+fi
+
+echo
+echo "# metante novajn sekretojn..."
+echo ${access_token} | docker secret create voko-afido.access_token -
+#cat ${keyfile_pub} | docker secret create voko-afido.ssh_key.pub -
+cat ${keyfile_github} | docker secret create voko-afido.github_key -
+#echo "smtp.provizanto.org" | docker secret create voko-afido.smtp_server -
+#echo "redaktoservo@provizanto.org" | docker secret create voko-afido.smtp_user -
+#echo "M14P4$svort0" | docker secret create voko-afido.smtp_password -
+echo "tomocero" | docker secret create voko-afido.smtp_server -
+echo "tomocero@tomocero" | docker secret create voko-afido.smtp_user -
+echo "sekreta" | docker secret create voko-afido.smtp_password -
+
+docker secret ls --filter name=voko-afido. 
+
+# Uzante la procesumon voko-tomocero por forsendo de retpoŝtoj vi anstataŭe donu por smtp
+# la sekvan agordon. La avantaĝo estas, ke tomocero transsendas la retpoŝtojn en serioj
+# kaj tiel ne por ĉiu unuopa retpoŝto okazas rekonekto, kion kelkaj provizantoj
+# malhelpas per kelkminuta deviga paŭzo.
+#   echo "voko-tomocero" | docker secret create voko-afido.smtp_server -
+#   echo "" | docker secret create voko-afido.smtp_user -
+#   echo "" | docker secret create voko-afido.smtp_password -
+
+
