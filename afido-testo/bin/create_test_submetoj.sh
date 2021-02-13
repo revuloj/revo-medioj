ff5c085473784b1cdec9808b1132fa1e1960c989
Wolfram Diestel <diestel@steloj.de>
2021-02-13 21:37:01 +0100

    test por submetoj

---
+++ afido-testo/bin/create_test_submetoj.sh
@@ -0,0 +1,273 @@
+#!/bin/bash
+
+db=db314802x3159000
+usr=s314802_3159000
+pwd=sekreta
+tmp=subm_tmp.sql
+
+cat <<EOQ > ${tmp}
+DELETE FROM submeto;
+
+INSERT INTO submeto(sub_email,sub_cmd,sub_state,sub_desc,sub_fname,sub_content) 
+VALUES ('${test_redaktanto}','redakto','nov','testa ŝanĝo de cxval','cxeval',
+'<?xml version="1.0"?>
+<!DOCTYPE vortaro SYSTEM "../dtd/vokoxml.dtd">
+<vortaro>
+<art mrk="\$Id: cxeval.xml,v 1.74 2019/07/03 08:10:19 revo Exp \$">
+<kap>
+  <ofc>*</ofc><rad>&ccirc;eval</rad>/o <fnt><bib>PV</bib></fnt>
+</kap>
+<drv mrk="cxeval.0o">
+  <kap><ofc>*</ofc><tld/>o</kap>
+  <snc mrk="cxeval.0o.genro">
+    <uzo tip="fak">ZOO</uzo>
+    <dif>
+      <ref tip="super" cel="speci.0o.BIO">Specio</ref> 
+      (<trd lng="la">Equus caballus</trd>) de grandaj
+      <ref tip="lst" cel="huf.nepar0ulo"
+      lst="voko:neparhufuloj">neparhufulaj</ref>
+      <ref tip="malprt" cel="mam.0uloj">mamuloj</ref> uzataj ekde
+      la antikva epoko kiel rajd- kaj tir-bestoj:
+      <ekz>
+        azenoj, zebroj, kaj <tld/>oj estas en la sama genro;
+      </ekz>
+      <ekz>
+        &ccirc;iuj <tld/>oj kaj &ccirc;aroj de Faraono kaj liaj
+        rajdantoj kaj lia militistaro atingis ilin
+        <fnt><bib>MT</bib>, <lok>&Eli; 14:9</lok></fnt>.
+      </ekz>
+    </dif>
+    <trd lng="bg">&c_k;&c_o;&c_n;</trd>
+    <trd lng="cs">k&uring;&ncaron;</trd>
+    <trdgrp lng="de">
+      <trd>Pferd</trd>,
+      <trd>Gaul<klr> [mezokcidentgermane]</klr></trd>,
+      <trd>Ross<klr> [sudgermane]</klr></trd>
+    </trdgrp>
+    <trdgrp lng="el">
+      <trd>&alfa_ton;&lambda;&omikron;&gamma;&omikron;</trd>,
+      <trd>&jota_ton;&pi;&pi;&omikron;&sigma_fina;</trd>
+    </trdgrp>
+    <trd lng="en">horse</trd>
+    <trdgrp lng="ru">
+      <trd>&c_k;&c_o;&c_n;&c_mol;</trd>,
+      <trd>&c_l;&c_o;&c_sh;&c_a;&c_d;&c_mol;</trd>
+    </trdgrp>
+    <trd lng="tr">at</trd>
+    <trd lng="vo">jevod</trd>
+  </snc>
+  <snc mrk="cxeval.0o.sxaka">
+    <uzo tip="fak">SPO</uzo>
+    <dif>
+      <ref tip="super" cel="sxak.0peco">&Scirc;akpeco</ref>, 
+      kutime figurata per <tld/>a kapo.
+    </dif>
+    <ref tip="sin" cel="kavali.0o.SPO">kavaliro</ref>
+    <trd lng="de">Springer</trd>
+    <trd lng="en">knight</trd>
+    <trd lng="he"><ind>&pe;&resh;&shin;</ind><klr tip="amb">
+        (&shin;&het;&mem;&tet;)</klr></trd>
+    <trdgrp lng="hu">
+      <trd>l&oacute;</trd>,
+      <trd>husz&aacute;r</trd>
+    </trdgrp>
+    <trd lng="ru">&c_k;&c_o;&c_n;&c_mol;</trd>
+  </snc>
+  <trd lng="be">&c_k;&c_o;&c_n;&c_mol;</trd>
+  <trd lng="ca">cavall</trd>
+  <trdgrp lng="zh">
+    <trd>&#39532;</trd>,
+    <trd><klr>tr. </klr>&#39340; [<ind>m&#462;</ind>]</trd>
+  </trdgrp>
+</drv>
+<drv mrk="cxeval.0eto">
+  <kap><tld/>eto</kap>
+  <snc mrk="cxeval.0eto.etacxevalo">
+    <dif>
+      Malgranda <tld/>o.
+    </dif>
+    <trd lng="de">Pferdchen</trd>
+    <trd lng="en">pony</trd>
+    <trd lng="nl">paardje</trd>
+    <trdgrp lng="pl">
+      <trd>konik</trd>,
+      <trd>kucyk</trd>
+    </trdgrp>
+    <trdgrp lng="ru">
+      <trd>&c_l;&c_o;&c_sh;&c_a;&c_d;&c_k;&c_a;</trd>,
+      <trd>&c_k;&c_o;&c_n;&c_jo;&c_k;</trd>
+    </trdgrp>
+    <trd lng="vo">jevodil</trd>
+  </snc>
+  <snc mrk="cxeval.0eto.FIG">
+    <uzo tip="stl">FIG</uzo>
+    <dif>
+      <ref tip="super" cel="tem.0o">Temo</ref>, pri kiu koncernulo 
+      &ccirc;iam volonte <ref cel="parol.0i.diri">parolas</ref>, 
+      kiu &ccirc;iam lin <ref cel="intere.0i.per_scivolo">interesas</ref>:
+      <ekz>
+        tio estas lia amata <tld/>eto<fnt><bib>PrV</bib></fnt>;
+      </ekz>
+      <ekz>
+        li tuj ekrajdis sian amatan <tld/>eton pri la supersigna
+        reformo;
+      </ekz>
+      <ekz>
+        la facileco de <nac>Volap&uuml;k</nac> estas la plej amata <tld/>eto,
+        sur kiu ili elveturas al &ccirc;iu okazo
+        <fnt><aut>&Z;</aut>,
+        <lok>citite la&ubreve; PIV, p.158</lok>.
+        </fnt>
+      </ekz>
+    </dif>
+    <ref tip="sin" cel="am.0atajxo">amata&jcirc;o</ref>
+    <refgrp tip="vid">
+      <ref cel="sxat.0okupo">&scirc;atokupo</ref>.
+    </refgrp>
+    <trdgrp lng="de">
+      <trd>Steckenpferd</trd>,
+      <trd>Lieblingsbesch&auml;ftigung</trd>,
+      <trd>Hobby</trd>
+    </trdgrp>
+    <trd lng="en">hobbyhorse</trd>
+    <trdgrp lng="fr">
+      <trd>dada</trd>,
+      <trd>marotte</trd>
+    </trdgrp>
+    <trd lng="he">&shin;&yod;&gimel;&yod;&vav;&fnun;</trd>
+    <trd lng="hu">vessz&odblac;paripa</trd>
+    <trd lng="it"><ind>cavallo</ind> di battaglia<klr>
+    (fig.)</klr></trd>
+    <trdgrp lng="nl">
+      <trd>stokpaardje</trd>, <trd>hobby</trd>
+    </trdgrp>
+    <trd lng="pl">konik</trd>
+    <trd lng="ru">&c_k;&c_o;&c_n;&c_jo;&c_k;</trd>
+    <trd lng="sk">kon&iacute;&ccaron;ek</trd>
+  </snc>
+</drv>
+</art>
+<!--
+\$Log: cxeval.xml,v \$
+Revision 1.74  2019/07/03 08:10:19  revo
+A A: zh: -ind- anstataux ol -klr-
+
+Revision 1.73  2019/06/20 12:10:14  revo
+J V: + kur~o
+-->
+</vortaro>');
+
+INSERT INTO submeto(sub_email,sub_cmd,sub_state,sub_desc,sub_fname,sub_content) 
+VALUES ('${test_redaktanto}','redakto','nov','testa ŝanĝo de abel','abel',
+'<?xml version="1.0"?>
+<!DOCTYPE vortaro SYSTEM "../dtd/vokoxml.dtd">
+<vortaro>
+<art mrk="\$Id: abel.xml,v 1.77 2019/06/30 07:47:08 revo Exp \$">
+<kap>
+  <ofc>*</ofc><rad>abel</rad>/o
+</kap>
+<drv mrk="abel.0o">
+  <kap><ofc>*</ofc><tld/>o</kap>
+  <snc mrk="abel.0o.ZOO">
+    <uzo tip="fak">ZOO</uzo>
+    <dif>
+      <ref tip="lst" cel="genr.0o.BIO" lst="voko:zoologiaj_genroj">Genro</ref>
+       el la familio
+      <ref tip="malprt" cel="abeled.0oj"><tld/>edoj</ref>
+      (<trd lng="la">Apis</trd>), el kiu unu specio
+      (<trd lng="la">Apis mellifera</trd>) estas bredata
+      pro la <ref cel="miel.0o">mielo</ref>:
+      <ekz>
+        kiu havas <tld/>ojn, havas mielon
+        <fnt>
+          <bib>PrV</bib>
+        </fnt>;
+      </ekz>
+      <ekz>
+        ili pelis vin tiel, kiel faras la <tld/>oj
+        <fnt>
+          <bib>MT</bib>
+          <lok>Readmono 1:44</lok>
+        </fnt>;
+      </ekz>
+      <ekz>
+        jen svarmamaso da <tld/>oj
+        <fnt>
+          <bib>MT</bib>
+          <lok>Ju&gcirc;istoj 14:8</lok>
+        </fnt>.
+      </ekz>
+    </dif>
+    <bld lok="&WCU;/thumb/1/1d/European_honey_bee_extracts_nectar.jpg/304px-European_honey_bee_extracts_nectar.jpg" prm="PD">E&ubreve;ropa mielabelo (Apis mellifera) prenante nektaron el asterofloro
+         <fnt>
+           <aut>John Severns (Severnjc)</aut>
+           <lok><url ref="&WCW;/File:European_honey_bee_extracts_nectar.jpg">&WCW;/File:European_honey_bee_extracts_nectar.jpg</url></lok>
+         </fnt>
+    </bld>
+    <trd lng="th">&#3612;&#3638;&#3656;&#3591;</trd>
+  </snc>
+  <rim>
+    <ref tip="hom" cel="abel1.0o">Abelo</ref>
+    nomi&gcirc;as anka&ubreve; norvega matematikisto.
+  </rim>
+  <trd lng="be">&c_p;&c_ch;&c_a;&c_l;&c_a;</trd>
+  <trd lng="bg">&c_p;&c_ch;&c_je;&c_l;&c_a;</trd>
+  <trd lng="sv">bi</trd>
+  <trd lng="uk">&c_b;&c_d;&c_zh;&c_o;&c_l;&c_a;</trd>
+  <trdgrp lng="tr">
+    <trd>bal ar&inodot;s&inodot;</trd>,
+    <trd>ar&inodot;</trd>
+  </trdgrp>
+  <trd lng="vo">bien</trd>
+</drv>
+<drv mrk="abel.0ujo">
+  <kap><tld/>ujo</kap>
+  <snc mrk="abel.0ujo.HOR">
+    <uzo tip="fak">HOR</uzo>
+    <dif>
+      Dometo konstruita de <tld/>oj, a&ubreve; korbo a&ubreve; kesteto
+      konstruita de homo por lo&gcirc;igi <tld/>ojn:
+      <ekz>
+        en <tld/>ujon ne blovu
+        <klr>(ne incitu koleremulon)</klr>
+        <fnt><bib>PrV</bib></fnt>;
+      </ekz>
+      <ekz>
+        <tld/>ujon ne incitu, amason ne spitu
+        <fnt>
+          <bib>PrV</bib>
+        </fnt>.
+      </ekz>
+    </dif>
+  </snc>
+  <trd lng="be">&c_v;&c_u;&c_l;&c_je;&c_j;</trd>
+  <trd lng="bg">&c_k;&c_o;&c_sh;&c_je;&c_r;</trd>
+  <trd lng="ja">&#12511;&#12484;&#12496;&#12481;&#12398;&#24035;&#31665;
+  [<ind>&#12415;&#12388;&#12400;&#12385;&#12398;&#12377;&#12400;&#12371;</ind>]</trd>
+  <trd lng="nl">bijenkorf</trd>
+  <trdgrp lng="th">
+    <trd>&#3585;&#3621;&#3656;&#3629;&#3591;&#3648;&#3621;&#3637;&#3657;&#3618;&#3591;&#3612;&#3638;&#3657;&#3591;</trd>,
+    <trd>&#3621;&#3633;&#3591;&#3648;&#3621;&#3637;&#3657;&#3618;&#3591;&#3612;&#3638;&#3657;&#3591;</trd>,
+    <trd>&#3627;&#3637;&#3610;&#3648;&#3621;&#3637;&#3657;&#3618;&#3591;&#3612;&#3638;&#3657;&#3591;</trd>
+  </trdgrp>
+  <trd lng="tr">kovan</trd>
+  <trd lng="vo">bienadomil</trd>
+</drv>
+</art>
+<!--
+\$Log: abel.xml,v \$
+Revision 1.77  2019/06/30 07:47:08  revo
+adaptis URL de Monato
+
+Revision 1.76  2019/04/22 12:10:15  revo
+W B: aldonis tajan tradukon
+
+Revision 1.75  2019/03/01 10:10:20  revo
+W D: pliaj ekz-oj, rim hom Abelo (matematikisto)
+-->
+</vortaro>');
+
+SELECT sub_id,sub_email,sub_state,sub_desc FROM submeto;
+EOQ
+
+mysql --user=${usr} --password=${pwd} ${db} < ${tmp}
---
+++ afido-testo/test-preparo-mysql.bash
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+stack=afidotesto
+
+### Ĉu Afido ktp. estas aktivaj? Ni bezonas ĝian n-ron...
+abelo_id=$(docker ps --filter name=${stack}_abelo -q) && echo "abelo: ${abelo_id}"
+
+# Plibonigu: kiel ni povas montri tion en "bats"?
+if [ "${abelo_id}" = "" ]; then echo "abelo ne aktiva!" 1>&2; exit 1; fi
+
+docker cp $(pwd)/bin/create_test_submetoj.sh ${abelo_id}:/
+docker exec -it ${abelo_id} bash -c "/create_test_submetoj.sh"
+
+
+
+
