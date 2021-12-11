477b09ad13c665a6105fd718ab5f019643679655
Wolfram Diestel <diestel@steloj.de>
2021-12-11 11:17:07 +0100

    sintakskorekto, +remove-orphans

--- cetoniujo/bin/cetoniujo-start.sh
+++ cetoniujo/bin/cetoniujo-start.sh
@@ -3,5 +3,5 @@
 env_file=${HOME}/etc/.envfile
 compose_file=docker-compose-srv.yml
 
-docker-compose -f ${compose_file} --env-file {$ENV_FILE} up -d
+docker-compose -f ${compose_file} --env-file {$ENV_FILE} up -d --remove-orphans
 docker-compose -f ${compose_file} logs -f
