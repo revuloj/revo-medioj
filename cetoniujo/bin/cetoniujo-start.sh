dc2052d9e674beacb2e4d36e429a4619ce9410e4
Wolfram Diestel <diestel@steloj.de>
2021-12-11 10:39:17 +0100

    agordo de medivariabloj por docker-compose

--- cetoniujo/bin/cetoniujo-start.sh
+++ cetoniujo/bin/cetoniujo-start.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+env_file=${HOME}/etc/.envfile
 compose_file=docker-compose-srv.yml
-docker-compose -f ${compose_file} up -d
+
+docker-compose -f ${compose_file} --env-file {$ENV_FILE} up -d
 docker-compose -f ${compose_file} logs -f
