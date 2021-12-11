60a1f249c3f601fa3d393fca6e033531b0841ebd
Wolfram Diestel <diestel@steloj.de>
2021-12-11 11:27:08 +0100

    -pli da sintakseraroj

--- cetoniujo/bin/cetoniujo-start.sh
+++ cetoniujo/bin/cetoniujo-start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-env_file=${HOME}/etc/.envfile
+env_file=${HOME}/etc/.env
 compose_file=docker-compose-srv.yml
 
-docker-compose -f ${compose_file} --env-file {$ENV_FILE} up -d --remove-orphans
+docker-compose -f ${compose_file} --env-file ${env_file} up -d --remove-orphans
 docker-compose -f ${compose_file} logs -f
