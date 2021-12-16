e000e57bc9a2884cf8d790a811eda9171088dd7f
Wolfram Diestel <diestel@steloj.de>
2021-12-16 20:49:30 +0100

    env

--- cetoniujo/bin/cetoniujo-pull.sh
+++ cetoniujo/bin/cetoniujo-pull.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+env_file=../../etc/.env
 compose_file=docker-compose-srv.yml
-docker-compose -f ${compose_file} pull
-docker-compose -f ${compose_file} images
+
+docker-compose -f ${compose_file} --env-file ${env_file} pull
+docker-compose -f ${compose_file} --env-file ${env_file} images
