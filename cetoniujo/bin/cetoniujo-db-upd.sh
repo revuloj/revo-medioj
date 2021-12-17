67e2ade2e247d25aa7ec75f5d65b9eb310d19f8a
Wolfram Diestel <wolfram.diestel@t-systems.com>
2021-12-17 14:44:23 +0100

    env_file

--- cetoniujo/bin/cetoniujo-db-upd.sh
+++ cetoniujo/bin/cetoniujo-db-upd.sh
@@ -3,7 +3,8 @@
 # vi povas doni kiel argumenton 'update-db' aŭ 'redaktantoj'
 # se vi ne donas argumenton ni supozas 'update-db'
 target="${1:-update-db}"
+env_file=../../etc/.env
 compose_file=docker-compose-srv.yml
 
-docker-compose -f ${compose_file} exec cetonio bin/instalo.sh ${target}
+docker-compose -f ${compose_file} --env-file ${env_file} exec cetonio bin/instalo.sh ${target}
 
--- cetoniujo/bin/cetoniujo-stop.sh
+++ cetoniujo/bin/cetoniujo-stop.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+env_file=../../etc/.env
 compose_file=docker-compose-srv.yml
-docker-compose -f ${compose_file} down
+
+docker-compose -f ${compose_file} --env-file ${env_file} down
