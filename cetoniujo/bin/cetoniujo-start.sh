e43256a082d496113795e48935c7d276b0eaffa0
Wolfram Diestel <wolfram.diestel@t-systems.com>
2021-12-17 16:35:35 +0100

    +env_file

--- cetoniujo/bin/cetoniujo-start.sh
+++ cetoniujo/bin/cetoniujo-start.sh
@@ -4,4 +4,4 @@ env_file=../../etc/.env
 compose_file=docker-compose-srv.yml
 
 docker-compose -f ${compose_file} --env-file ${env_file} up -d --remove-orphans
-docker-compose -f ${compose_file} logs -f
+docker-compose -f ${compose_file} --env-file ${env_file} logs -f
