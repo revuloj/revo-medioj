35bd17615e3afcd8889fd7eb05b99dd923bbf1d2
Wolfram Diestel <wolfram.diestel@t-systems.com>
2022-01-19 16:45:39 +0100

    kor. test-preparo

--- araneujo/tst/test-preparo.bash
+++ araneujo/tst/test-preparo.bash
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 araneo_id=$(docker ps --filter name=araneujo_araneo -q)
-araneo_host=localhost
+araneo_host=0.0.0.0
 ports=$(docker service ls -f name=araneujo_araneo --format '{{.Ports}}')
 ip_port=${ports%->*}
 pnum=${ip_port#*:}
