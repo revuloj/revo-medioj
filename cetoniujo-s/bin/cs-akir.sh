cb2dac2dc4bb4abdc7a9356116823c4b13422d20
Wolfram Diestel <wolfram.diestel@t-systems.com>
2023-06-08 14:01:02 +0200

    + *akir.sh

--- cetoniujo-s/bin/cs-akir.sh
+++ cetoniujo-s/bin/cs-akir.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
-echo "Ni ŝargas la procezujojn por Cetoniujo el la deponejo ĉe Github (Ghcr)"
+label="${1:-latest}"
 
-docker pull ghcr.io/revuloj/voko-cetonio/voko-cetonio
-docker pull ghcr.io/revuloj/voko-grilo/voko-grilo
-docker pull ghcr.io/revuloj/voko-akrido/voko-akrido
-docker pull ghcr.io/revuloj/voko-cikado/voko-cikado
+echo "Ni ŝargas la procezujojn por Cetoniujo el la deponejo ĉe Github (Ghcr), etikedo: '${label}'"
+
+docker pull ghcr.io/revuloj/voko-cetonio/voko-cetonio:${label}
+docker pull ghcr.io/revuloj/voko-grilo/voko-grilo:${label}
+docker pull ghcr.io/revuloj/voko-akrido/voko-akrido:${label}
+docker pull ghcr.io/revuloj/voko-cikado/voko-cikado:${label}
 
 # alinomu por loka uzo
-docker tag ghcr.io/revuloj/voko-cetonio/voko-cetonio voko-cetonio
-docker tag ghcr.io/revuloj/voko-grilo/voko-grilo voko-grilo
-docker tag ghcr.io/revuloj/voko-akrido/voko-akrido voko-akrido
-docker tag ghcr.io/revuloj/voko-cikado/voko-cikado voko-cikado
+docker tag ghcr.io/revuloj/voko-cetonio/voko-cetonio:${label} voko-cetonio
+docker tag ghcr.io/revuloj/voko-grilo/voko-grilo:{label} voko-grilo
+docker tag ghcr.io/revuloj/voko-akrido/voko-akrido:${label} voko-akrido
+docker tag ghcr.io/revuloj/voko-cikado/voko-cikado:${label} voko-cikado
+
