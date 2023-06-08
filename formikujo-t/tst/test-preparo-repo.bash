0cd16992b8513db420d469511a3a7a6fdf599d74
Wolfram Diestel <wolfram.diestel@t-systems.com>
2023-06-08 15:17:17 +0200

    test-repo

--- formikujo-t/tst/test-preparo-repo.bash
+++ formikujo-t/tst/test-preparo-repo.bash
@@ -9,17 +9,21 @@ formiko_id=$(docker ps --filter name=${stack}_formiko -q) && echo "Formiko: ${fo
 # Plibonigu: kiel ni povas montri tion en "bats"?
 #if [ "${afido_id}" = "" ]; then echo "Afido ne aktiva!" 1>&2; exit 1; fi
 #if [ "${tomocero_id}" = "" ]; then echo "Tomocero ne aktiva!" 1>&2; exit 1; fi
-if [ "${formiko_id}" = "" ]; then echo "Formiko ne aktiva!" 1>&2; exit 1; fi
+if [ "${formiko_id}" = "" ]; then 
+    echo "Formiko ne aktiva!" 1>&2
+    exit 1
+else
+    docker exec -it ${formiko_id} bash -c "rm -rf /home/formiko/tmp/inx_tmp/*"
+    docker exec -it ${formiko_id} bash -c "rm -rf /home/formiko/revo/art"
+    docker exec -it ${formiko_id} bash -c "rm -rf /home/formiko/revo/hst"
+    docker exec -it ${formiko_id} bash -c "rm -rf /home/formiko/revo/inx"
+    docker exec -it ${formiko_id} bash -c "rm -rf /home/formiko/revo/xml"
+    docker exec -it ${formiko_id} bash -c "rm -rf /home/formiko/revo/tez"
+    docker exec -it ${formiko_id} bash -c "rm -rf /home/formiko/tgz/*"
+    docker exec -it ${formiko_id} bash -c "rm -rf /home/formiko/revo-fonto"
+    docker cp tst/create_test_repo.sh ${formiko_id}:/usr/local/bin/
+    docker exec -it ${formiko_id} bash -c "create_test_repo.sh"
+    docker exec -u1001 -it ${formiko_id} bash -c "git clone ./test-repo revo-fonto"
+fi
 
-docker exec -it ${formiko_id} bash -c "rm -rf /home/formiko/tmp/inx_tmp/*"
-docker exec -it ${formiko_id} bash -c "rm -rf /home/formiko/revo/art"
-docker exec -it ${formiko_id} bash -c "rm -rf /home/formiko/revo/hst"
-docker exec -it ${formiko_id} bash -c "rm -rf /home/formiko/revo/inx"
-docker exec -it ${formiko_id} bash -c "rm -rf /home/formiko/revo/xml"
-docker exec -it ${formiko_id} bash -c "rm -rf /home/formiko/revo/tez"
-docker exec -it ${formiko_id} bash -c "rm -rf /home/formiko/tgz/*"
-docker exec -it ${formiko_id} bash -c "rm -rf /home/formiko/revo-fonto"
-docker cp create_test_repo.sh ${formiko_id}:/usr/local/bin/
-docker exec -it ${formiko_id} bash -c "create_test_repo.sh"
-docker exec -u1001 -it ${formiko_id} bash -c "git clone ./test-repo revo-fonto"
 
