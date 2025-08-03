e5146c21fb29d77cf7c71fe792c9cf318cc9f3be
Wolfram Diestel <diestel@steloj.de>
2025-08-03 15:16:12 +0200

    voko-afido plu nur kiel efemera prozeco (tasko), rearanĝo de afidotesto

--- afido-t/test-preparo-repo.bash
+++ afido-t/test-preparo-repo.bash
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# set -x
+
 #if [[ -z "$TEST_RETADRESO" ]]; then
 #  echo "Vi devas antaŭdifini variablon TEST_RETADRESO por doni unu retadreson kien sendiĝas raportoj dum testoj."
 #  exit 1
@@ -8,23 +10,27 @@
 stack=afidotesto
 
 ### Ĉu Afido ktp. estas aktivaj? Ni bezonas ĝian n-ron...
-afido_id=$(docker ps --filter name=${stack}_afido -q) && echo "afido: ${afido_id}"
+# afido_id=$(docker ps --filter name=${stack}_afido -q) && echo "afido: ${afido_id}"
 
 # Plibonigu: kiel ni povas montri tion en "bats"?
-if [ "${afido_id}" = "" ]; then echo "afido ne aktiva!" 1>&2; exit 1; fi
+# if [ "${afido_id}" = "" ]; then echo "afido ne aktiva!" 1>&2; exit 1; fi
 
-docker cp bin/create_test_repo.sh ${afido_id}:/usr/local/bin/
-docker cp bin/create_test_gist.sh ${afido_id}:/usr/local/bin/
+# ni uzas volumes: en docker-compose anstatataŭe
+#docker cp bin/create_test_repo.sh ${afido_id}:/usr/local/bin/
+#docker cp bin/create_test_gist.sh ${afido_id}:/usr/local/bin/
 
-docker exec -it -u1074 ${afido_id} bash -c "rm -rf /home/afido/dict/gists"
-docker exec -it -u1074 ${afido_id} bash -c "rm -rf /home/afido/dict/xml"
-docker exec -it -u1074 ${afido_id} bash -c "rm -rf /home/afido/dict/tmp"
-docker exec -it -u1074 ${afido_id} bash -c "rm -rf /home/afido/dict/log"
-docker exec -it -u1074 ${afido_id} bash -c "rm -rf /home/afido/dict/revo-fonto"
-docker exec -it -u1074 ${afido_id} bash -c "rm -rf /home/afido/test-repo"
+# tion ni jam faras en test-preparo, do ne denove tie ĉi?
+# docker exec -it -u1074 ${afido_id} bash -c "rm -rf /home/afido/dict/gists"
+# docker exec -it -u1074 ${afido_id} bash -c "rm -rf /home/afido/dict/xml"
+# docker exec -it -u1074 ${afido_id} bash -c "rm -rf /home/afido/dict/tmp"
+# docker exec -it -u1074 ${afido_id} bash -c "rm -rf /home/afido/dict/log"
+# docker exec -it -u1074 ${afido_id} bash -c "rm -rf /home/afido/dict/revo-fonto"
+# docker exec -it -u1074 ${afido_id} bash -c "rm -rf /home/afido/test-repo"
 
-docker exec -it -u1074 ${afido_id} bash -c "create_test_repo.sh"
-docker exec -it -u1074 ${afido_id} bash -c "create_test_gist.sh ${TEST_RETADRESO}"
+#docker exec -it -u1074 ${afido_id} bash -c "create_test_repo.sh"
+#docker exec -it -u1074 ${afido_id} bash -c "create_test_gist.sh ${TEST_RETADRESO}"
+
+docker compose run --rm afido create_test_repo.sh
 
 
 
--- afido-t/test-preparo.bash
+++ afido-t/test-preparo.bash
@@ -1,19 +1,29 @@
 #!/bin/bash
 
+#set -x
+
 stack=afidotesto
 
 ### Ĉu Afido ktp. estas aktivaj? Ni bezonas ĝian n-ron...
-afido_id=$(docker ps --filter name=${stack}_afido -q) && echo "afido: ${afido_id}"
+#afido_id=$(docker ps --filter name=${stack}_afido -q) && echo "afido: ${afido_id}"
 
 # Plibonigu: kiel ni povas montri tion en "bats"?
-if [ "${afido_id}" = "" ]; then echo "afido ne aktiva!" 1>&2; exit 1; fi
+#if [ "${afido_id}" = "" ]; then echo "afido ne aktiva!" 1>&2; exit 1; fi
 
-docker exec -it -u1074 ${afido_id} bash -c "rm -rf /home/afido/dict/gists"
-docker exec -it -u1074 ${afido_id} bash -c "rm -rf /home/afido/dict/xml"
-docker exec -it -u1074 ${afido_id} bash -c "rm -rf /home/afido/dict/tmp"
-docker exec -it -u1074 ${afido_id} bash -c "rm -rf /home/afido/dict/log"
-docker exec -it -u1074 ${afido_id} bash -c "rm -rf /home/afido/test-repo"
-docker exec -it -u1074 ${afido_id} bash -c "rm -rf /home/afido/revo-fonto"
+# docker exec -it -u1074 ${afido_id} bash -c "rm -rf /home/afido/dict/gists"
+# docker exec -it -u1074 ${afido_id} bash -c "rm -rf /home/afido/dict/xml"
+# docker exec -it -u1074 ${afido_id} bash -c "rm -rf /home/afido/dict/tmp"
+# docker exec -it -u1074 ${afido_id} bash -c "rm -rf /home/afido/dict/log"
+# docker exec -it -u1074 ${afido_id} bash -c "rm -rf /home/afido/test-repo"
+# docker exec -it -u1074 ${afido_id} bash -c "rm -rf /home/afido/revo-fonto"
+
+## docker service update --args "rm -rf /home/afido/dict/gists /home/afido/dict/xml /home/afido/dict/tmp /home/afido/dict/log /home/afido/test-repo /home/afido/revo-fonto" ${stack}_afido
+## 
+## docker service scale ${stack}_afido=1
+## 
+## docker service update --rollback ${stack}_afido
+
+docker compose run --rm --entrypoint="" --remove-orphans afido rm -rf /home/afido/dict/gists /home/afido/dict/xml /home/afido/dict/tmp /home/afido/dict/log /home/afido/test-repo /home/afido/revo-fonto
 
 
 
